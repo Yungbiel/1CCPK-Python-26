4f88f2b13f299e0e16c4b4fa8bace75d8fc5306a
Yungbiel <gabrielkanequi@gmail.com>
2026-03-16 09:53:45 -0300

    aula 02 - variaveis

---
+++ aual02a-variaveis.py
@@ -0,0 +1,40 @@
+print("Olá, Mundo!")
+
+print(7 + 4) #concatenando strings
+
+#para comentários em python utiliza #
+'''
+comentário 
+de multiplás linhas
+'''
+
+nome = ("Gabriel")
+idade = 18
+peso = 77.8
+
+print(nome, idade, peso)
+
+# input- utilizado para receber dados do usuário
+
+nome = input("digite seu nome: ")
+idade = input("digite sua idade: ")
+peso = input("digite seu peso: ")
+
+print(nome, idade, peso)
+
+
+
+
+
+nome = input("digite seu nome: ")
+
+print (f"olá, bem vindo {nome}")
+
+
+dia = input("digite o seu dia de nascimento: ")
+mês = input("digite seu mÊs de nascimento: ")
+ano = input("digite seu ano de nascimento: ")
+
+print(dia, mês, ano)
+
+print(15 + 20)
