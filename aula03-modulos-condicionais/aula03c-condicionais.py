84a5e1b18b59740b4a5841499f0b897493ed69ef
aula 03 <gabrielkanequi@gmail.com>
2026-03-30 09:44:04 -0300

    aula 03 modulos, função e condicionais

---
+++ aula03-modulos-condicionais/aula03c-condicionais.py
@@ -0,0 +1,27 @@
+# nota = 7.0
+#
+# if nota < 6:
+#     prit(f"Sua nota é: {nota}")
+#
+# print("FIM")
+
+# nota = 5.0
+#
+# if nota < 6:
+#     print("Reprovado")
+# else:
+#     print("Aprovado")
+#
+# print("FIM")
+
+nota = 5.9
+
+if nota < 4:
+    print("Reprovado")
+else:
+    if nota < 6:
+        print("Recuperação")
+    else:
+        print("Aprovado")
+
+print("FIM")
